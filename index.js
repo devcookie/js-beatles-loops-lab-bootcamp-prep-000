a19e0b55b6741a27fee17a86c8c9274c96b018b3
Catherine Lee <lee.catherine77@gmail.com>
2018-04-13 19:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   //var counter = 0
-  for(var i = 0; i <= musicians.length; i++) {
+  for(var i = 0; i <= musicians.length-1; i++) {
     array.push(musicians[i] + ' plays ' + instruments[i])
   }
   return array
